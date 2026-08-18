95203992f318f6721fa759fb6c0ddc68f9c76406
Leonardo Rosler <roslerleonardo@gmail.com>
2026-08-17 22:06:33 -0300

    corrige rota de teste dos usuários

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { prisma } from "../lib/prisma.js";
-import { app } from "../app.js";
 
 const usuariosRoutes = Router();
 
-app.get("/", async (req, res)=>{
+usuariosRoutes.get("/", async (req, res)=>{
     const usuarios = await prisma.usuario.findMany()
-
     res.json(usuarios)
 })
 
